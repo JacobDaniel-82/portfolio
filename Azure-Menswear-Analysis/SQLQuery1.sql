6b08e85d26773bc2124796825b2627f0c97ee615
Jacob Daniel R <jacobdanielr82@gmail.com>
2026-01-06 14:14:30 +0530

    Add files via upload

---
+++ Azure-Menswear-Analysis/SQLQuery1.sql
@@ -0,0 +1,20 @@
+
+
+select * from [dbo].[Men Tshirt]
+
+update [dbo].[Men Tshirt]
+set [Original_Price] = trim(replace(cast([Original_Price] as varchar(max)),'?',''))
+where [Original_Price] like'%?%'
+
+
+update [dbo].[Men Tshirt]
+set [Sale_Price] = trim(replace(cast([Sale_Price] as varchar(max)),'?',''))
+where [Sale_Price] like'%?%'
+
+
+
+
+
+
+
+
